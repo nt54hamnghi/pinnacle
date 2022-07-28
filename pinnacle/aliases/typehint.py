53f374b8a23111b2ee7c17882ffe7c8189eb0712
nt54hamnghi <hamnghi250699@gmail.com>
2022-07-27 22:15:47 -0400

    remove invalid imports

--- pinnacle/aliases/typehint.py
+++ pinnacle/aliases/typehint.py
@@ -1,12 +1,9 @@
 from pathlib import Path
 from typing import Optional, TypeVar, Union
 
-from brownie.network.contract import Contract, ProjectContract
 
 RealNumber = Union[int, float]
 
 NoneableStr = Optional[str]  # optional string
 GeneralPath = Union[Path, str]  # general path
 GeneralT = TypeVar("GeneralT")  # generic type var
-
-ContractBase = Union[ProjectContract, Contract]
