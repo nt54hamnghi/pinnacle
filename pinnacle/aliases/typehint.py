c0d4e627f87ba2b89bbbc252890a31b370c27766
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-02 14:41:09 -0400

    move type aliases to a seperated file

--- pinnacle/aliases/typehint.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from typing import Optional, TypeVar, Union
-
-RealNumber = Union[int, float]
-
-NoneableStr = Optional[str]  # optional string
-GeneralPath = Union[Path, str]  # general path
-GeneralT = TypeVar("GeneralT")  # generic type var
---
+++ pinnacle/type_aliases.py
@@ -0,0 +1,4 @@
+from os import PathLike
+from typing import TypeAlias
+
+PathType: TypeAlias = PathLike | str
