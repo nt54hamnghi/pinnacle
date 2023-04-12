131bbef662e606b87f5d723e0f76840d97f40722
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-12 17:23:18 -0400

    update constants

--- pinnacle/consts/dirs.py
+++ pinnacle/consts/dirs.py
@@ -1,6 +1,6 @@
 import pathlib
 
 # directory configs
-ROOT_DIR = pathlib.Path(__file__).parents[1]
+ROOT_DIR = pathlib.Path(__file__).parents[2]
 METADATA_DIR = ROOT_DIR / "metadata"
 IMG_DIR = ROOT_DIR / "img"
--- pinnacle/consts/pin_service.py
+++ pinnacle/consts/pin_service.py
@@ -1,1 +1,2 @@
-PINATA_BASE_URL = "https://api.pinata.cloud"
+LOCAL_SERVICE = "http://127.0.0.1:5001/api/v0"
+PINATA_SERVICE = "https://api.pinata.cloud"
