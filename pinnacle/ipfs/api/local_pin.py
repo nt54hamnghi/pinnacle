be92609f6cf33647bf170424bc202e4fbae70fa2
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-14 14:05:10 -0400

    add AsyncLocalPin

--- pinnacle/ipfs/api/local_pin.py
+++ pinnacle/ipfs/api/local_pin.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+
 import httpx
+import psutil
 from pydantic import BaseModel, Field
 
 from pinnacle.ipfs.config import Config
 from pinnacle.ipfs.content.content import Content
+from pinnacle.ipfs.model.model import Delegates, Pin, PinStatus, Status
 
-from .pin_api import PinAPI
+from .pin_api import AsyncPinAPI, PinAPI
 
 
 class NoIPFSDaemonError(Exception):
@@ -18,27 +22,64 @@ class LocalPinAddResponse(BaseModel):
     Bytes: int | None = None
 
 
-class LocalPin(PinAPI):
+class LocalPinBase:
     global_config = Config()
 
+    @staticmethod
+    def ipfs_daemon_active() -> bool:
+        """Check if ipfs daemon is running"""
+        return "ipfs" in {p.name() for p in psutil.process_iter()}
+
+    def _add(
+        self, content: Content, raw_response: httpx.Response
+    ) -> PinStatus:
+        raw_response.raise_for_status()
+
+        response = LocalPinAddResponse(**raw_response.json())
+        content.pinned(response.Hash)
+
+        return PinStatus(
+            requestid=response.Hash,
+            status=Status.pinned,
+            created=datetime.now(),
+            pin=Pin(cid=response.Hash, name=content.basename),
+            delegates=Delegates.from_list(["localhost"]),
+        )
+
+
+class LocalPin(LocalPinBase, PinAPI):
     def add(
         self,
         content: Content,
         *,
         cid_version: int = 1,
     ):
-        try:
-            raw = self._post(
-                "add",
-                files=content.prepare(),
-                query_params={"cid-version": cid_version},
-            )
-            raw.raise_for_status()
-        except httpx.ConnectError:
+        if not self.ipfs_daemon_active():
             raise NoIPFSDaemonError("IPFS daemon is not running")
-        else:
-            response = LocalPinAddResponse(**raw.json())
-            pinned_cid = response.Hash
-            content.pinned(pinned_cid)
 
-            return response.dict()
+        raw = self._post(
+            "add",
+            files=content.prepare(),
+            query_params={"cid-version": cid_version},
+        )
+
+        return self._add(content, raw)
+
+
+class AsyncLocalPin(LocalPinBase, AsyncPinAPI):
+    async def add(
+        self,
+        content: Content,
+        *,
+        cid_version: int = 1,
+    ):
+        if not self.ipfs_daemon_active():
+            raise NoIPFSDaemonError("IPFS daemon is not running")
+
+        raw = await self._post(
+            "add",
+            files=content.prepare(),
+            query_params={"cid-version": cid_version},
+        )
+
+        return self._add(content, raw)
--- pinnacle/main.py
+++ pinnacle/main.py
@@ -1,15 +1,30 @@
+from pprint import pprint
+
+import anyio
+
 from pinnacle.consts.dirs import IMG_DIR
-from pinnacle.ipfs.api.local_pin import LocalPin
-from pinnacle.ipfs.content.content import Content
+from pinnacle.ipfs.api.local_pin import AsyncLocalPin, LocalPin
+from pinnacle.ipfs.content.content import AsyncContent, Content
 
 
 def main():
     with LocalPin() as pin:
         with Content(IMG_DIR / "han.png") as content:
-            pin.add(content)
-
+            pin_status = pin.add(content)
             print(content.gateway("local"))
+
+            return pin_status
+
+
+async def async_main():
+    async with AsyncLocalPin() as apin:
+        async with AsyncContent(IMG_DIR / "kai.png") as acontent:
+            pin_status = await apin.add(acontent)
+            print(acontent.gateway("local"))
+
+            return pin_status
 
 
 if __name__ == "__main__":
-    main()
+    pprint(main().json(exclude_none=True))
+    pprint(anyio.run(async_main).json(exclude_none=True))
