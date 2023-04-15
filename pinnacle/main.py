d85b019f0333046af07c6f37471bb0786b2f856a
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-15 13:05:26 -0400

    re-organize main

--- pinnacle/main.py
+++ pinnacle/main.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from typing import Callable
 
 import anyio
 
@@ -10,27 +11,65 @@ from pinnacle.ipfs.api.pinata import AsyncPinata, Pinata
 from pinnacle.ipfs.content.content import AsyncContent, Content
 
 
-def main(Pinner: type[PinAPI]):
-    with Pinner() as pin:
+def pin(Pinner: type[PinAPI], env: str | None = None, verbose: bool = True):
+    with Pinner() as pin_api:
+
+        if not pin_api.authless:
+            pin_api.authenticate_with_env(env)
+
         with Content(IMG_DIR / "han.png") as content:
-            pin_status = pin.add(content)
-            print(content.gateway("local"))
+            pin_status = pin_api.add(content)
+
+            if verbose:
+                print("\n", content.gateway("local"), end="\n\n")
 
             return pin_status
 
 
-async def async_main(Pinner: type[AsyncPinAPI]):
-    async with Pinner() as apin:
-        async with AsyncContent(IMG_DIR / "kai.png") as acontent:
-            pin_status = await apin.add(acontent)
-            print(acontent.gateway("local", "subdomain"))
+async def async_pin(
+    Pinner: type[AsyncPinAPI], env: str | None = None, verbose: bool = True
+):
+    async with Pinner() as pin_api:
+
+        if not pin_api.authless:
+            pin_api.authenticate_with_env(env)
+
+        async with AsyncContent(IMG_DIR / "kai.png") as content:
+            pin_status = await pin_api.add(content)
+
+            if verbose:
+                print("\n", content.gateway("local"), end="\n\n")
 
             return pin_status
+
+
+def local():
+    response = pin(LocalPin)
+    pprint(response)
+
+    async_response = anyio.run(async_pin, AsyncLocalPin)
+    pprint(async_response)
+
+
+def pinata():
+    response = pin(Pinata, env="PINATA_JWT")
+    pprint(response)
+
+    async_response = anyio.run(async_pin, AsyncPinata, "PINATA_JWT")
+    pprint(async_response)
+
+
+def nft_storage():
+    response = pin(NFTStorage, env="NFT_STORAGE_JWT")
+    pprint(response)
+
+    async_response = anyio.run(async_pin, AsyncNFTStorage, "NFT_STORAGE_JWT")
+    pprint(async_response)
+
+
+def main(func: Callable):
+    func()
 
 
 if __name__ == "__main__":
-    response = main(NFTStorage)
-    pprint(response)
-
-    response = anyio.run(async_main, AsyncNFTStorage)
-    pprint(response)
+    main(pinata)
