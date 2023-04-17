9920b63f81158ca26d9b622cbdb7d55d286d7866
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-17 19:07:21 -0400

    add tests for AsyncContent

---
+++ tests/ipfs/content/test_async_content.py
@@ -0,0 +1,61 @@
+from io import UnsupportedOperation
+from pathlib import Path
+from typing import Literal, TypeAlias
+
+import pytest
+
+from pinnacle.constants import IMG_DIR
+from pinnacle.ipfs.content.content import AsyncContent
+
+
+@pytest.fixture
+def img_path():
+    filename = "han.png"
+    return IMG_DIR / filename, filename
+
+
+ImgPathFixture: TypeAlias = tuple[Path, Literal["han.png"]]
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture
+def async_content(img_path):
+    path, _ = img_path
+    return AsyncContent(path)
+
+
+@pytest.mark.anyio
+async def test_async_content(async_content: AsyncContent):
+    try:
+        await async_content.open()
+    except:
+        pass
+    else:
+        assert async_content.opened
+        assert async_content._bytes is not None
+    finally:
+        await async_content.close()
+
+
+@pytest.mark.anyio
+async def test_async_content_context_manager(img_path: ImgPathFixture):
+    path, _ = img_path
+
+    async with AsyncContent(path) as content:
+        assert content.opened
+        assert content._bytes is not None
+
+    assert content.closed
+
+
+@pytest.mark.anyio
+async def test_async_reopen_fail(img_path: ImgPathFixture):
+    path, _ = img_path
+
+    async with AsyncContent(path) as content:
+        with pytest.raises(UnsupportedOperation):
+            await content.open()
